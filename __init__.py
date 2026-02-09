1e29743d36115da7e2e115fa33b1fa074553a712
GangYang-gy <gangy.yang@mail.utoronto.ca>
2026-02-08 22:30:43 -0500

    Restore root __init__.py content

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+SeisFMTD: Seismic Full Moment Tensor Determination
+
+A Python package for seismic full moment tensor inversion using 
+Hamiltonian Monte Carlo (HMC) methods.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Gang Yang"
+
+from . import pyCAPLunar
+from . import pyCAPSolvers
+from . import MTTools
